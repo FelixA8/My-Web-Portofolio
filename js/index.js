e0e0d3b6fb4857be8bd6e47bbd05cfea4e713bc2
FelixAnderson <felix.anderson1504@gmail.com>
2024-02-11 21:49:18 +0700

    update 11-02-2024

--- js/index.js
+++ js/index.js
@@ -5,3 +5,46 @@ const headerEl = document.querySelector(".header");
 btnNav.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+$(".num").ready(function () {
+  var lines = 15;
+  var textLength = 40;
+
+  for (var i = 0; i < lines; i++) {
+    $(".num").first().clone().insertAfter($(".num").last());
+  }
+
+  setInterval(function () {
+    $(".num").each(function () {
+      if ($(this).text().length < textLength) {
+        $(this).text($(this).text() + Math.round(Math.random()));
+      } else {
+        $(this).text(
+          Math.round(Math.random()) +
+            $(this)
+              .text()
+              .substring(0, $(this).text().length - 1)
+        );
+      }
+    });
+  }, 95);
+});
+
+$("main").hide();
+$("header").hide();
+$("footer").hide();
+
+const btnAbout = document.querySelector(".aboutme-button");
+console.log(btnNav);
+const showAbout = document.querySelector(".header");
+const showCode = document.querySelector(".loading-screen");
+
+btnAbout.addEventListener("click", function () {
+  showAbout.classList.add("fixed");
+  $("main").show();
+  $("header").show();
+  $("footer").show();
+  $(".loading-screen").hide("slow", function () {
+    $(".loading-screen").remove();
+  });
+});
